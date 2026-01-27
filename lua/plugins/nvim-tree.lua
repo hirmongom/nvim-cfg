a58c9860b5b843710b39727bb54d54905052957c
Hiram Montejano Gomez <hirmongom@outlook.com>
2026-01-27 09:33:30 +0100

    Added keymaps for nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -38,6 +38,15 @@ require("nvim-tree").setup {
     vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
     vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close directory"))
     vim.keymap.set("n", "r", api.tree.reload, opts("Refresh"))
+    vim.keymap.set("n", "n", api.fs.create, opts("New file/dir"))
+    vim.keymap.set("n", "f", api.fs.rename, opts("Rename node"))
+    vim.keymap.set("n", "d", api.fs.remove, opts("Remove node"))
+    vim.keymap.set("n", "c", api.fs.copy.node, opts("Copy node into tree.clipboard"))
+    vim.keymap.set("n", "x", api.fs.cut, opts("Cut node into tree.clipboard"))
+    vim.keymap.set("n", "v", api.fs.paste, opts("Paste node from tree.clipboard"))
+    vim.keymap.set("n", "q", api.fs.clear_clipboard, opts("Clear tree.clipboard"))
+    vim.keymap.set("n", "p", api.fs.print_clipboard, opts("Print tree.clipboard"))
+    vim.keymap.set("n", "g", api.commands.get, opts("Retrieve all commands"))
   end,
 }
 
