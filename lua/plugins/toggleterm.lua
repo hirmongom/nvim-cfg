42bc43fe2ab8b4c1a9b1f78de87e5788d7bbaffd
Hiram Montejano Gomez <hirmongom@outlook.com>
2026-03-03 10:33:19 +0100

    Added keymap for easier return to normal mode

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,5 +1,13 @@
 require("toggleterm").setup {
-  open_mapping = "<C-Space>t",
-  direction = "horizontal",
+  -- open_mapping = "<C-Space>t",
+  open_mapping = "<leader>t",
+  direction = "tab",
   insert_mappings = false,
+  terminal_mappings = false,
+
+  on_open = function(term)
+    vim.keymap.set("t", "<C-q>", [[<C-\><C-n>]], { 
+      buffer = term.bufnr, 
+      desc = "Return to Normal mode" })
+  end,
 }
